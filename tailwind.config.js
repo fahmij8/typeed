2c36e43bdfcbfd968ebd2f66572ab14434e4fd07
fahmij8 <fahmijabbar12@gmail.com>
2022-12-03 23:20:24 +0700

    chore: initialize tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['media', 'class'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        sans: ['var(--font-nunito)'],
+      },
+    },
+  },
+  plugins: [],
+};
