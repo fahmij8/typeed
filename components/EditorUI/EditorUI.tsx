7d8d057fec8529d94bb70c80c749f7d57c030c03
fahmij8 <fahmijabbar12@gmail.com>
2022-12-06 03:32:26 +0700

    feat: retain input to localStorage

--- components/EditorUI/EditorUI.tsx
+++ components/EditorUI/EditorUI.tsx
@@ -11,12 +11,21 @@ import { Slate, Editable, withReact } from "slate-react";
 import { withHistory } from "slate-history";
 import { toggleMark, initialValue } from "./utilities";
 import { HOTKEYS } from "./keyboardEvents";
-import { useState } from "react";
 import type { CustomElement } from "@/lib/types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const EditorUI = () => {
-  const [notes, setNotes] = useState(initialValue);
+  const [initialNotes, setInitialNotes] = useState<CustomElement[] | null>(
+    null
+  );
+  useEffect(() => {
+    const content = localStorage.getItem("content");
+    if (content) {
+      setInitialNotes(JSON.parse(content));
+    } else {
+      setInitialNotes(initialValue);
+    }
+  }, []);
   const editor = useMemo(
     () => withLayout(withHistory(withReact(createEditor()))),
     []
@@ -34,52 +43,60 @@ const EditorUI = () => {
     []
   );
 
-  useEffect(() => {
-    const storeNotesDebounced = setTimeout(() => {
-      const isAstChange = editor.operations.some(
-        (op) => "set_selection" !== op.type
-      );
-      if (isAstChange) {
-        const content = JSON.stringify(notes);
-        localStorage.setItem("content", content);
-      }
-    }, 1000);
-
-    return () => clearTimeout(storeNotesDebounced);
-  }, [editor.operations, notes]);
-
   return (
-    <Slate
-      editor={editor}
-      value={notes}
-      onChange={(value) => {
-        setNotes(value as CustomElement[]);
-      }}
-    >
-      <HoveringToolbar />
-      <Editable
-        spellCheck
-        placeholder="Enter your notes here..."
-        renderLeaf={EditorLeaf}
-        renderElement={EditorElement}
-        onKeyDown={(event) => {
-          for (const pressedKey in HOTKEYS) {
-            if (isHotkey(pressedKey, event)) {
-              const hotkey = HOTKEYS[pressedKey as keyof typeof HOTKEYS];
-              if (hotkey.type === "mark" && typeof hotkey.mark === "string") {
-                event.preventDefault();
-                toggleMark(editor, hotkey.mark as CustomElement["type"]);
-              } else if (
-                hotkey.type === "special" &&
-                typeof hotkey.mark === "function"
-              ) {
-                hotkey.mark(editor, event);
-              }
+    <>
+      {!initialNotes && (
+        <div className="flex items-center justify-center p-5">
+          <div className="flex space-x-2 animate-pulse">
+            <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+          </div>
+        </div>
+      )}
+      {initialNotes && (
+        <Slate
+          editor={editor}
+          value={initialNotes}
+          onChange={(value) => {
+            const isAstChange = editor.operations.some(
+              (op) => "set_selection" !== op.type
+            );
+            if (isAstChange) {
+              const content = JSON.stringify(value);
+              localStorage.setItem("content", content);
             }
-          }
-        }}
-      />
-    </Slate>
+          }}
+        >
+          <HoveringToolbar />
+          <Editable
+            spellCheck
+            placeholder="Enter your notes here..."
+            renderLeaf={EditorLeaf}
+            renderElement={EditorElement}
+            onKeyDown={(event) => {
+              for (const pressedKey in HOTKEYS) {
+                if (isHotkey(pressedKey, event)) {
+                  const hotkey = HOTKEYS[pressedKey as keyof typeof HOTKEYS];
+                  if (
+                    hotkey.type === "mark" &&
+                    typeof hotkey.mark === "string"
+                  ) {
+                    event.preventDefault();
+                    toggleMark(editor, hotkey.mark as CustomElement["type"]);
+                  } else if (
+                    hotkey.type === "special" &&
+                    typeof hotkey.mark === "function"
+                  ) {
+                    hotkey.mark(editor, event);
+                  }
+                }
+              }
+            }}
+          />
+        </Slate>
+      )}
+    </>
   );
 };
 
