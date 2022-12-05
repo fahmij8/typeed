80f4d65bf93476c0d9abd762fc079ed232cc863d
fahmij8 <fahmijabbar12@gmail.com>
2022-12-06 04:42:38 +0700

    feat: add more tips

--- lib/constant/index.ts
+++ lib/constant/index.ts
@@ -2,5 +2,8 @@ export const TIPS = [
   "Press CTRL or CMD + B to toggle text to bold",
   "Press CTRL or CMD + I to toggle text to italic",
   "Press CTRL or CMD + U to toggle text to underline",
+  "Press CTRL or CMD + ` to toggle code block",
   "Select text to view the toolbar",
+  "Checking Show toolbar on focus make sure the toolbar is always visible",
+  "You'll now it's saved when you see the status in the bottom right corner",
 ];
