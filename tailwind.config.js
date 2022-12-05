bd8875dddb719d475e2ccb86aa1c08e26ed86f5a
fahmij8 <fahmijabbar12@gmail.com>
2022-12-06 04:42:30 +0700

    chore: add custom fade in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,29 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-nunito)"],
       },
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "15%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "90%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-up": "fade-in-up 3.5s ease-out 1",
+      },
     },
   },
   plugins: [],
