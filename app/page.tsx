21107f9e4838c839c15cb59151da7433cfd00242
fahmij8 <fahmijabbar12@gmail.com>
2022-12-05 21:37:21 +0700

    feat: add usefull tip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BottomSheetUI from "@/components/BottomSheetUI";
 import EditorUI from "@/components/EditorUI";
 import Header from "@/components/Header";
+import { TIPS } from "@/lib/constant";
 
 export default function Home() {
   return (
@@ -10,6 +11,10 @@ export default function Home() {
         <section className="grid max-sm:mx-4 max-w-2xl mx-auto p-4 mt-4 border-2 rounded-lg bg-white shadow-2 dark:bg-gray-500">
           <EditorUI />
         </section>
+        <p className="text-xs text-slate-700/50 dark:text-white text-center mt-3">
+          {" "}
+          Tip of the day : {TIPS[Math.floor(Math.random() * TIPS.length)]}
+        </p>
       </main>
       <BottomSheetUI />
     </>
---
+++ lib/constant/index.ts
@@ -0,0 +1,6 @@
+export const TIPS = [
+  "Press CTRL or CMD + B to toggle text to bold",
+  "Press CTRL or CMD + I to toggle text to italic",
+  "Press CTRL or CMD + U to toggle text to underline",
+  "Select text to view the toolbar",
+];
